6956e7e55bd37c3b0c1f9c6f0b30c67a1f8be284
Anatoliy <anatolvereschaka@ukr.net>
2024-11-02 10:41:26 +0200

    changes

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,6 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;  //(union type)
+
+let literal: "enable" | "disable";  //(literal type)
